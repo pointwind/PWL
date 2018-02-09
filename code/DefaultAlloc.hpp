b8edebcdcc381e79679c69b86c903c3f45cf772c
pointwind <Point_Wind@163.com>
2018-02-09 10:14:38 +0800

    alloc

---
+++ code/DefaultAlloc.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include<new>
+#include <cstddef>
+namespace PWL {
+	class DefaultAlloc
+	{
+		void* operator()(std::size_t size)
+		{
+			return ::operator new(size,std::nothrow);
+		}
+		void operator()(std::size_t* ptr)
+		{
+			 ::operator delete(ptr);
+		}
+	};
+}
