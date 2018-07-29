017ff2c6e7609090965ee6f5c03db75115da0646
pointwind <Point_Wind@163.com>
2018-07-29 23:44:48 +0800

    update map

--- code/map.h
+++ code/map.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "functional.h"
+#include"util.h"
 namespace PWL
 {
-	template<class T,class U>class Map
+	template<class Key, class T, class Compare = Less<Key>>
+	class Map
 	{
+	public:
+		using key_type = Key;
+		using map_type = T;
+		using value_type = PWL::pair<key_type, map_type>;
+		using key_compare = Compare;
+	public:
+
 
 	};
 }
